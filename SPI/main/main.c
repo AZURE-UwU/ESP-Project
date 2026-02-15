1338bf1eaed82afb4e5f410538aca162bb60d3c7
AZURE-UwU <wbw20060318@163.coom>
2026-02-15 21:28:50 +0800

    ST7789

--- SPI/main/main.c
+++ SPI/main/main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+
+
+
+
+
+/*
+ST7789 -> ESP32 S2
+
+
+Fixed pin mapping:
+SCL -> GPIO12 FSPICLK
+SDA -> GPIO11 FSPID
+CS -> GPIO10 FSPICS0
+
+Unfixed pin mapping:
+DC -> GPIO13 FSPID
+RES -> GPIO9 FSPIHD
+
+
+
+
+*/
+
+
+
 void app_main(void)
 {
 
