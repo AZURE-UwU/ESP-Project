6d03f089b99e931e3164a39a36585ea2bde4f158
AZURE-UwU <wbw20060318@163.coom>
2026-02-15 23:25:08 +0800

    移植

--- SPI/main/main.c
+++ SPI/main/main.c
@@ -8,18 +8,15 @@
 /*
 ST7789 -> ESP32 S2
 
-
 Fixed pin mapping:
 SCL -> GPIO12 FSPICLK
 SDA -> GPIO11 FSPID
 CS -> GPIO10 FSPICS0
 
 Unfixed pin mapping:
-DC -> GPIO13 FSPID
-RES -> GPIO9 FSPIHD
-
-
-
+DC -> GPIO13
+RES -> GPIO9
+BLK -> GPIO8
 
 */
 
